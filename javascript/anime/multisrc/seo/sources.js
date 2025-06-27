119b06c8a41e1b1e81db68939ec26b8aa352aea7
xMohnad <mohnad00967@gmail.com>
2025-06-27 14:08:27 +0000

    feat(anime): add SEO sources for WIT ANIME and Anime4Up (AR)

---
+++ javascript/anime/multisrc/seo/sources.js
@@ -0,0 +1,27 @@
+// prettier-ignore
+const mangayomiSources = [
+  {
+    "name": "WIT ANIME",
+    "lang": "ar",
+    "baseUrl": "https://witanime.uno",
+    "apiUrl": "",
+    "iconUrl": "https://witanime.uno/wp-content/uploads/2023/08/cropped-Logo-WITU-192x192.png",
+    "typeSource": "single",
+    "itemType": 1,
+    "version": "0.0.1",
+    "isNsfw": false,
+    "pkgPath": "anime/multisrc/seo/seo.js"
+  },
+  {
+    "name": "Anime4Up",
+    "lang": "ar",
+    "baseUrl": "https://anime4up.rest",
+    "apiUrl": "",
+    "iconUrl": "https://anime4up.rest/wp-content/uploads/2025/02/images.png",
+    "typeSource": "single",
+    "itemType": 1,
+    "version": "0.0.1",
+    "isNsfw": false,
+    "pkgPath": "anime/multisrc/seo/seo.js"
+  }
+];
